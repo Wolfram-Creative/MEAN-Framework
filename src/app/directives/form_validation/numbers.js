28d1386add8b51e797283e53edff3994f8c76f96
Brian Noah <brianjoshuanoah@gmail.com>
2014-01-25 14:06:41 -0800

    form validation directives

---
+++ src/app/directives/form_validation/numbers.js
@@ -0,0 +1,37 @@
+app.directive('integer', function() {
+    var INTEGER_REGEXP = /^\-?\d+$/;
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+                if (INTEGER_REGEXP.test(viewValue)) {
+                    // it is valid
+                    ctrl.$setValidity('integer', true);
+                    return viewValue;
+                } else {
+                    // it is invalid, return undefined (no model update)
+                    ctrl.$setValidity('integer', false);
+                    return undefined;
+                }
+            });
+        }
+    };
+});
+ 
+app.directive('smartFloat', function() {
+    var FLOAT_REGEXP = /^\-?\d+((\.|\,)\d+)?$/;
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+                if (FLOAT_REGEXP.test(viewValue)) {
+                    ctrl.$setValidity('float', true);
+                    return parseFloat(viewValue.replace(',', '.'));
+                } else {
+                    ctrl.$setValidity('float', false);
+                    return undefined;
+                }
+            });
+        }
+    };
+});
